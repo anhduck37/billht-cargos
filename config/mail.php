5609af39dc9ad19a0c56c6043e82460aef2bef58
QuangTV <quangtv@hamsa.co>
2021-11-25 00:16:04 +0700

    debug

--- config/mail.php
+++ config/mail.php
@@ -20,4 +20,13 @@ return [
         ],
     ],
     'log_channel' => env('MAIL_LOG_CHANNEL'),
+    'stream' => [
+        'ssl' => [
+            'allow_self_signed' => true,
+            'verify_peer'       => false,
+            'verify_peer_name'  => false,
+        ],
+    ],
+
+
 ];
