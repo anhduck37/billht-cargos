b9df390cadd4d1aec768eabdc2a401ecc5488000
QuangTV <quangtv@hamsa.co>
2021-11-29 08:39:05 +0700

    fix bug

--- resources/views/template/print.blade.php
+++ resources/views/template/print.blade.php
@@ -324,7 +324,7 @@
                             <div class="card-body">
                                 <h4 class="card-title">Họ tên, địa chỉ người nhận: </h4>
                                 <p class="card-text size-text">{{isset($order->receiver) ? $order->receiver->receiver_name : '.....'}}</p>
-                                <p class="card-text"><small class="text-muted size-text">{{isset($order->receiver) ? (isset($order->receiver->address) ? $order->receiver->address . ', ' : '') . (isset($order->receiver->ward) ? $order->receiver->ward->ward_name . ', ' : '')  . (isset($order->sender->district) ? $order->sender->district->district_name . ', ' : '')  . (isset($order->sender->city) ? $order->sender->city->city_name : '') : '.....'}} </small></p>
+                                <p class="card-text"><small class="text-muted size-text">{{isset($order->receiver) ? (isset($order->receiver->address) ? $order->receiver->address . ', ' : '') . (isset($order->receiver->ward) ? $order->receiver->ward->ward_name . ', ' : '')  . (isset($order->receiver->district) ? $order->receiver->district->district_name . ', ' : '')  . (isset($order->receiver->city) ? $order->receiver->city->city_name : '') : '.....'}} </small></p>
 
                                 <p class="card-text"><h4 class="card-title">Điện thoại:</h4> {{isset($order->receiver) ? $order->receiver->receiver_phone : '.....'}}</p>
                             </div>
