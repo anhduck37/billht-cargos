39b42cf56b7644077159099ca4bc432603072f1a
Quang Tran <quangtran@x3english.com>
2022-10-28 00:34:46 +0700

    webcam

--- resources/views/orders/edit.blade.php
+++ resources/views/orders/edit.blade.php
@@ -27,7 +27,7 @@
                             @if(in_array(auth()->user()->level, [\App\User::LEVEL_ADMIN, \App\User::LEVEL_STAFF]))
                             <button type="button" data-toggle="modal" data-target="#openModalEmail" class="btn btn-primary mb-2">Gửi email</button>
                             @endif
-                            <button type="button" id="openCamera" class="btn btn-primary mb-2">Upload image</button>
+                            <button type="button" id="openCamera" class="btn btn-primary mb-2">Image</button>
                             <a class='btn btn-light mb-2' href="{{route('orders.index')}}">Thoát</a>
                         </div>
 
