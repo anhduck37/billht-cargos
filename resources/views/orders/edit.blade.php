a34966d1e8e5db99dd6bcdecf39bd6b811f07806
Quang Tran <quangtran@x3english.com>
2022-11-13 19:49:33 +0700

    fix

--- resources/views/orders/edit.blade.php
+++ resources/views/orders/edit.blade.php
@@ -27,7 +27,7 @@
                             @if(in_array(auth()->user()->level, [\App\User::LEVEL_ADMIN, \App\User::LEVEL_STAFF]))
                             <button type="button" data-toggle="modal" data-target="#openModalEmail" class="btn btn-primary mb-2">Gửi email</button>
                             @endif
-                            @if(@if(auth()->user()->level != \App\User::LEVEL_USER))
+                            @if(auth()->user()->level != \App\User::LEVEL_USER)
                             <button type="button" id="image" class="btn btn-primary mb-2">Chụp ảnh</button>
                             @endif
                             <a class='btn btn-light mb-2' href="{{route('orders.index')}}">Thoát</a>
