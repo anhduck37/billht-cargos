d0b6903d77a9ceae118e2fbfb81e3972e975ba34
Quang Tran <quangtran@x3english.com>
2022-12-14 08:45:56 +0700

    add calculate image

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -32,7 +32,17 @@ class ViewServiceProvider extends ServiceProvider
             if(isset($auth->user()->id)) {
                 $start_time = date('Y-m-d');
                 $end_time = (new DateTime($start_time))->modify('+1 day')->format('Y-m-d');
-                $totalOrder = Order::where('user_id', $auth->user()->id)->where('created_at', '>=', date('Y-m-d'))->where('created_at', '<', $end_time)->count();
+                $totalOrder = Order::join('order_images', 'order_images.order_id', '=', 'orders.id')
+                            ->where('orders.user_id', $auth->user()->id)
+                            ->where(function($q) use ($start_time, $end_time) {
+                                $q->where(function($t) use ($start_time, $end_time) {
+                                    $t->where('orders.created_at', '>=', $start_time)
+                                    ->where('orders.created_at', '<', $end_time);
+                                })->orWhere(function($h) use ($start_time, $end_time) {
+                                    $h->where('order_images.updated_at', '>=', $start_time)
+                                    ->where('order_images.updated_at', '<', $end_time);
+                                });
+                            })->count();
             }
             $view->with('totalOrder', $totalOrder);
         });
