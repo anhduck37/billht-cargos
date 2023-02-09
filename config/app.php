6d551223bb52dbcebfebcb8595989446ce3a2d06
Quang Tran <quangtran@x3english.com>
2023-02-09 14:20:35 +0700

    gg drive

--- config/app.php
+++ config/app.php
@@ -176,7 +176,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
-        App\Providers\ViewServiceProvider::class
+        App\Providers\ViewServiceProvider::class,
+        App\Providers\GoogleDriveServiceProvider::class
     ],
 
     /*
