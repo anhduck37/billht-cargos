f346982e3fc92498dc58dfe19723ae26a6bbc825
anhduck37 <anhduck37@gmail.com>
2023-05-03 09:13:46 +0700

    edit text print

--- resources/views/template/print.blade.php
+++ resources/views/template/print.blade.php
@@ -212,7 +212,7 @@
                             </div>
                             <div class="col-md-5">
                                 <h4 class="card-title">Thông tin giao nhận</h4>
-                                <p class="card-text size-text">Ngày gửi:..............</p>
+                                <p class="card-text size-text">Ngày phát:..............</p>
                                 <p class="card-text size-text">Nv phát:..............</p>
                                 <p class="card-text size-text" style="margin-bottom: 100px">Ký ghi rõ họ tên người nhận</p>
                             </div>
