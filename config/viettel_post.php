3c4a590b754ae2e1f6e43b24cba1467be68fa7c5
quangtv6 <quangtv6@msb.com.vn>
2024-06-07 15:46:44 +0700

    viettel post

--- config/viettel_post.php
+++ config/viettel_post.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'username' => env('VIETTEL_POST_USERNAME', '0984383097'),
-    'password' => env('VIETTEL_POST_PASSWORD', '0984383097'),
+    'username' => env('VIETTEL_POST_USERNAME', '0816881888'),
+    'password' => env('VIETTEL_POST_PASSWORD', '62851995ht'),
     'url'   => env('VIETTEL_POST_URL', 'https://partner.viettelpost.vn')
 ];
