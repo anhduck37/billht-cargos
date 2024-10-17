83304c484b8f06086219c81115cda8225803b9d1
quangtv6 <quangtv6@msb.com.vn>
2024-10-17 20:53:16 +0700

    debug

--- app/Services/ViettelPostService.php
+++ app/Services/ViettelPostService.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Services;
 
 use App\City;
@@ -11,7 +12,8 @@ use App\Service;
 use GuzzleHttp\Client;
 use GuzzleHttp\RequestOptions;
 
-class ViettelPostService {
+class ViettelPostService
+{
 
     private $username;
     private $password;
@@ -22,15 +24,17 @@ class ViettelPostService {
     private $api;
     private $service_viettel;
 
-    public function __construct($service_viettel=null) {
+    public function __construct($service_viettel = null)
+    {
         $this->username = config('viettel_post.username');
         $this->password = config('viettel_post.password');
         $this->url = config('viettel_post.url');
         $this->api = config('viettel_post.api');
         $this->service_viettel = $service_viettel;
     }
-    
-    public function refreshToken() {
+
+    public function refreshToken()
+    {
         $path = '/v2/user/Login';
         $client = new Client([
             'headers' => $this->headers
@@ -45,11 +49,11 @@ class ViettelPostService {
             ]
         );
         $result = json_decode($response->getBody()->getContents(), true);
-        if(empty($result['data'])) {
+        if (empty($result['data'])) {
             return $result;
         }
         $partnerConfig = PartnerConfig::where('partner_code', PartnerConfig::CODE_VIETTEL_POST)->first();
-        if(!$partnerConfig) {
+        if (!$partnerConfig) {
             $partnerConfig = new PartnerConfig();
             $partnerConfig->partner_code = PartnerConfig::CODE_VIETTEL_POST;
         }
@@ -58,17 +62,18 @@ class ViettelPostService {
         return $result;
     }
 
-    public function createOrder($order) {
+    public function createOrder($order)
+    {
         $path = '/v2/order/createOrder';
         $partnerConfig = PartnerConfig::where('partner_code', PartnerConfig::CODE_VIETTEL_POST)->first();
-        
+
         $this->headers['Token'] = $partnerConfig->token ?? '';
-        $senderAddress = ($order->sender->address ?? '') . ' ' .($order->sender->ward->ward_name ?? '') . ' ' . ($order->sender->city->city_name ?? '');
-        $receiverAddress = ($order->receiver->address ?? '') . ' ' .($order->receiver->ward->ward_name ?? '') . ' ' . ($order->receiver->city->city_name ?? '');
+        $senderAddress = ($order->sender->address ?? '') . ' ' . ($order->sender->ward->ward_name ?? '') . ' ' . ($order->sender->city->city_name ?? '');
+        $receiverAddress = ($order->receiver->address ?? '') . ' ' . ($order->receiver->ward->ward_name ?? '') . ' ' . ($order->receiver->city->city_name ?? '');
         $orderPayment = 1;
-        switch($order->payment_method) {
+        switch ($order->payment_method) {
             case Order::PAYMENT_METHOD_LAST:
-                if($order->collection > 0) {
+                if ($order->collection > 0) {
                     $orderPayment = 3;
                 }
                 break;
@@ -76,7 +81,7 @@ class ViettelPostService {
                 $orderPayment = 4;
                 break;
         }
-        if($order->collection > 0) $orderPayment = 3;
+        if ($order->collection > 0) $orderPayment = 3;
         $orderService = $this->getPriceAllNlp($order);
         $formatData = [
             "ORDER_NUMBER" => !empty($order->invoice_code) ? $order->invoice_code : $order->order_code,
@@ -96,7 +101,7 @@ class ViettelPostService {
             "RECEIVER_PROVINCE" => $order->receiver->city->city_code ?? 0,
             "PRODUCT_NAME" => $order->note,
             "PRODUCT_QUANTITY" => $order->quantity,
-            "PRODUCT_WEIGHT" =>$order->weight,
+            "PRODUCT_WEIGHT" => $order->weight,
             "PRODUCT_WIDTH" => $order->width,
             "PRODUCT_HEIGHT" => $order->height,
             "PRODUCT_LENGTH" => $order->long,
@@ -128,7 +133,7 @@ class ViettelPostService {
         $result = json_decode($response->getBody()->getContents(), true);
         $orderPartnerLog = new OrderPartnerLog();
         $status = OrderPartnerLog::STATUS_FAILD;
-        if(!empty($result['data'])) {
+        if (!empty($result['data'])) {
             $status = OrderPartnerLog::STATUS_SUCCESS;
             $order->order_partner_code = $result['data']['ORDER_NUMBER'] ?? null;
             $order->partner_code = Order::CODE_VIETTEL_POST;
@@ -144,24 +149,26 @@ class ViettelPostService {
         return $result;
     }
 
-    public function webhookTracking($data) {
+    public function webhookTracking($data)
+    {
         app(LogFileService::class)->writeLog('viettel_post', json_encode($data));
         $dataWebhook = isset($data['DATA']['ORDER_NUMBER']) ? $data['DATA'] : null;
-        if(!$dataWebhook) return;
+        if (!$dataWebhook) return;
 
         $order = Order::where('order_partner_code', $dataWebhook['ORDER_NUMBER'])->first();
-        if(!$order) {
+        if (!$order) {
             return;
         }
 
-        if(isset(PartnerConfig::MAP_STATUS_VIETTEL_POST[$dataWebhook['ORDER_STATUS']])) {
+        if (isset(PartnerConfig::MAP_STATUS_VIETTEL_POST[$dataWebhook['ORDER_STATUS']])) {
             $order->delivery_status = PartnerConfig::MAP_STATUS_VIETTEL_POST[$dataWebhook['ORDER_STATUS']];
             $order->save();
         }
         return;
     }
 
-    public function tracking($order) {
+    public function tracking($order)
+    {
         $path = '/api/setting/listOrderTrackingVTP3';
         $partnerConfig = PartnerConfig::where('partner_code', PartnerConfig::CODE_VIETTEL_POST)->first();
         $this->headers['token'] = $partnerConfig->token ?? '';
@@ -169,16 +176,18 @@ class ViettelPostService {
             'headers' => $this->headers
         ]);
 
-        $response = $client->get($this->api.$path, [
+        $response = $client->get($this->api . $path, [
             'query' => [
                 'OrderNumber' => $order->order_partner_code
             ]
         ]);
         $result = json_decode($response->getBody()->getContents(), true);
+        dd($result);
         return $result['data'] ?? null;
     }
 
-    private function formatDataWebhook($order, $data) {
+    private function formatDataWebhook($order, $data)
+    {
         $mapData = [
             'order_id' => $order->id ?? 0,
             'order_partner_code' => $data['ORDER_NUMBER'] ?? null,
@@ -198,10 +207,11 @@ class ViettelPostService {
         return $mapData;
     }
 
-    public function getPriceAllNlp($order, $orderService = true) {
+    public function getPriceAllNlp($order, $orderService = true)
+    {
         $path = '/v2/order/getPriceAllNlp';
-        $senderAddress = ($order->sender->address ?? '') . ' ' .($order->sender->ward->ward_name ?? '') . ' ' . ($order->sender->city->city_name ?? '');
-        $receiverAddress = ($order->receiver->address ?? '') . ' ' .($order->receiver->ward->ward_name ?? '') . ' ' . ($order->receiver->city->city_name ?? '');
+        $senderAddress = ($order->sender->address ?? '') . ' ' . ($order->sender->ward->ward_name ?? '') . ' ' . ($order->sender->city->city_name ?? '');
+        $receiverAddress = ($order->receiver->address ?? '') . ' ' . ($order->receiver->ward->ward_name ?? '') . ' ' . ($order->receiver->city->city_name ?? '');
         $formatData = [
             "SENDER_ADDRESS" => $senderAddress,
             "RECEIVER_ADDRESS" => $receiverAddress,
@@ -229,28 +239,30 @@ class ViettelPostService {
         return $orderService ? ($result['RESULT'][0]['MA_DV_CHINH'] ?? null) : $result;
     }
 
-    public function getGroupId($address) {
+    public function getGroupId($address)
+    {
         $replaceAddress = $this->stripVN($address);
         $address = strtoupper($replaceAddress);
         $groupId = null;
         $isBreak = false;
-        foreach(City::MAP_CITY_VIETTEL_POST as $city) {
-            foreach($city['city'] as $item) {
+        foreach (City::MAP_CITY_VIETTEL_POST as $city) {
+            foreach ($city['city'] as $item) {
                 $replaceItem = $this->stripVN($item);
-                if(str_contains($address, $replaceItem)) {
+                if (str_contains($address, $replaceItem)) {
                     $groupId = $city['group_id'];
                     $isBreak = true;
                     break;
                 }
             }
-            if($isBreak) {
+            if ($isBreak) {
                 break;
             }
         }
         return $groupId;
     }
 
-    public function stripVN($str) {
+    public function stripVN($str)
+    {
         $str = preg_replace("/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/", 'a', $str);
         $str = preg_replace("/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/", 'e', $str);
         $str = preg_replace("/(ì|í|ị|ỉ|ĩ)/", 'i', $str);
@@ -258,7 +270,7 @@ class ViettelPostService {
         $str = preg_replace("/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/", 'u', $str);
         $str = preg_replace("/(ỳ|ý|ỵ|ỷ|ỹ)/", 'y', $str);
         $str = preg_replace("/(đ)/", 'd', $str);
-    
+
         $str = preg_replace("/(À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ)/", 'A', $str);
         $str = preg_replace("/(È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ)/", 'E', $str);
         $str = preg_replace("/(Ì|Í|Ị|Ỉ|Ĩ)/", 'I', $str);
