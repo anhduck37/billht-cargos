6d21912fbf873a7e1e6c1bd26a52b13e0f9d59ea
quangtv6 <quangtv6@msb.com.vn>
2025-06-29 23:06:29 +0700

    add log query

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use File;
+use DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        DB::listen(function ($query) {
+            File::append(
+                storage_path('/logs/query.log'),
+                '[' . date('Y-m-d H:i:s') . '] time : ' . $query->time . " ms . " . PHP_EOL . $query->sql . ' [' . implode(', ', $query->bindings) . ']' . PHP_EOL . PHP_EOL
+            );
+        });
     }
 }
